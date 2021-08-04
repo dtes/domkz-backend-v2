357b664d1134130137b38202324e7d60839f5834
dtes <mygrnail@gmail.com>
2021-08-04 11:30:33 +0600

    --

--- src/test/java/kz/dom/domkzbackendv2/JdbcRepositoryTests.java
+++ src/test/java/kz/dom/domkzbackendv2/JdbcRepositoryTests.java
@@ -2,6 +2,7 @@ package kz.dom.domkzbackendv2;
 
 import kz.dom.domkzbackendv2.dto.HousingSearchFilterDTO;
 import kz.dom.domkzbackendv2.dto.HousingSearchResultDTO;
+import kz.dom.domkzbackendv2.model.PriceHistory;
 import kz.dom.domkzbackendv2.model.jdbc.Housing;
 import kz.dom.domkzbackendv2.model.jdbc.dict.housing.HousingType;
 import kz.dom.domkzbackendv2.model.jdbc.dict.location.Address;
@@ -18,9 +19,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Arrays;
+import java.util.Date;
 import java.util.Optional;
 
 import static kz.dom.domkzbackendv2.Printer.println;
@@ -62,10 +66,54 @@ public class JdbcRepositoryTests {
         println("\n=== search ===");
         HousingSearchFilterDTO filter = HousingSearchFilterDTO.builder().housingTypeId(2L).pageNum(1).pageSize(15).build();
         HousingSearchResultDTO result = housingSearchRepository.search(filter);
-        result.getData().forEach(item -> println(item));
+        result.getData().forEach(item -> println(item.getId() + ". " +item));
     }
 
-    // TODO: Сделать сохранение с @MappedCollection
+    @Test
+    @Commit
+    public void housingSave() {
+        PriceHistory ph1 = new PriceHistory();
+        ph1.setPrice(260_000d);
+        PriceHistory ph2 = new PriceHistory();
+        ph2.setPrice(270_000d);
+
+        Housing newHousing = new Housing();
+        newHousing.setRbdId(123001L);
+        newHousing.setDtCreate(new Date());
+        newHousing.setSupplySource(1L);
+        newHousing.setEmail("mygrnail@gmail.com");
+        newHousing.setContactName("Dias");
+        newHousing.setRoomCount(2);
+        newHousing.setFloorCount(5);
+        newHousing.setNum("324");
+        newHousing.setHouseNum("23");
+        newHousing.setEntryNumber("3");
+        newHousing.setTotalPrice(260_000d);
+        newHousing.setSell(true);
+        newHousing.setCorner(false);
+        newHousing.setPricePerSquareMeter(260_000d);
+        newHousing.setPriceHistories(Arrays.asList(ph1, ph2));
+
+        Housing saved = housingRepository.save(newHousing);
+        println(saved);
+    }
+
+    @Test
+    @Commit
+    public void housingUpdate() {
+        housingRepository.findById(269547L).ifPresent(housing -> {
+            housing.getPriceHistories().get(0).setPrice(265_000d);
+            housingRepository.save(housing);
+        });
+    }
+
+    @Test
+    public void housingDelete() {
+        housingRepository.findById(269547L).ifPresent(housing -> {
+            housingRepository.delete(housing);
+        });
+//        housingRepository.deleteById(269547L);
+    }
 
     @Test
     public void housingTypes() {
